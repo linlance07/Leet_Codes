5afad16234b091f93ecd30923fecf918ef479eef
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-03-29 12:33:10 +0530

    Time: 2188 ms (31.79%), Space: 44.4 MB (54.55%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,27 +1,15 @@
-class Solution(object):
-    def numOfMinutes(self, n, h, m, t):
-        """
-        :type n: int
-        :type headID: int
-        :type manager: List[int]
-        :type informTime: List[int]
-        :rtype: int
-        """
-        d=defaultdict(list)
-        if n==1:
-            return 0
-        for i in range(n):
-            d[m[i]].append(i)
-        #print(d)
-        r=d[-1][0]
-        q=[(t[r],r)]
-        m=-float("inf")
-        while q:
-            x,y=q.pop(0)
-            m=max(m,x)
-            for i in d[y]:
-                q.append((x+t[i],i))
-        return m
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        tree = defaultdict(list)
+        for i in range(len(manager)):
+            if manager[i]!=-1:
+                tree[manager[i]].append(i)
+        Q = [(informTime[headID],headID)]
+        time = -float('inf')
+        while Q:
+            a,b = Q.pop(0)
+            time = max(time,a)
+            for _ in tree[b]:
+                Q.append((informTime[_]+a,_))
+        return time
                 
-            
-        
