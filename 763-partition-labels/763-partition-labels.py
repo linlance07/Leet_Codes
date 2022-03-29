1a03e8fecab2b4f07b9ea245d5e868f6037260a7
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-03-29 18:59:03 +0530

    Time: 48 ms (72.69%), Space: 13.9 MB (72.99%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def partitionLabels(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        intervals = {}
+        for i in range(len(s)):
+            currentChar = s[i]
+            if currentChar in intervals:
+                intervals[currentChar][1] = i
+            else:
+                intervals[currentChar] = [i,i]
+        # extract the intervals
+        intervalsList = []
+        for index, item in intervals.items():
+            intervalsList.append(item)
+        mergedList = self.mergeIntervals(intervalsList)
+        return [y - x + 1 for x,y in mergedList]
+    
+    # overlapping intervals merge    
+    def mergeIntervals(self, listOfIntervals):
+        mergedIntervals = []
+        left = 0
+        right = 1
+        while right < len(listOfIntervals):
+            if listOfIntervals[left][1] > listOfIntervals[right][0]:
+                listOfIntervals[left][1] = max(listOfIntervals[left][1],listOfIntervals[right][1])
+                right+=1
+            else:
+                mergedIntervals.append(listOfIntervals[left])
+                left=right
+                right+=1
+        mergedIntervals.append(listOfIntervals[left])
+        return mergedIntervals
