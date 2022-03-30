f78d5a787f74b1bdf79d76faf902adddc28abf2b
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-03-30 09:07:33 +0530

    Time: 76 ms (99.97%), Space: 14.6 MB (86.99%) - LeetHub

---
+++ 707-design-linked-list/707-design-linked-list.py
@@ -0,0 +1,38 @@
+class MyLinkedList:
+
+    def __init__(self):
+        self.arr = []
+
+    def get(self, index: int) -> int:
+        try:
+            return self.arr[index]
+        except:
+            return -1
+
+    def addAtHead(self, val: int) -> None:
+        self.arr.insert(0,val)
+
+    def addAtTail(self, val: int) -> None:
+        self.arr.append(val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index<len(self.arr):
+            self.arr.insert(index,val)
+        elif index==len(self.arr):
+            self.arr.append(val)
+            
+
+    def deleteAtIndex(self, index: int) -> None:
+        try:
+            self.arr.pop(index)
+        except:
+            return
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
