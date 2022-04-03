a79bd88e74b1dfcc72019122f06a1718bfc83369
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-04-03 22:10:31 +0530

    Time: 40 ms (95.32%), Space: 13.8 MB (79.00%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,22 +1,21 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         l = None
-        for i in range(len(nums)-2,-1,-1):
-            if nums[i]<nums[i+1]:
-                l = i
-                break
         r = None
-        ma = 1000
+        for i in range(len(nums)-1,0,-1):
+            if nums[i-1]<nums[i]:
+                l = i - 1
+                break
         if l!=None:
+            ma = 1000
             for j in range(l+1,len(nums)):
                 if nums[j]>nums[l]:
                     if nums[j]<=ma:
-                        ma = nums[j]
                         r = j
             nums[l],nums[r] = nums[r],nums[l]
-        print(nums,l,r)
-        if l==None:
-            nums[:] = nums[::-1]
+            nums[:] = nums[:l+1] + (nums[l+1:])[::-1]            
         else:
-            nums[:] = list(nums[:l+1]) + list(nums[l+1:])[::-1]
+            nums[:] = nums[::-1]
+        
+                
         
