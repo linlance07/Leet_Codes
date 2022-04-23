affb67c8401a33f5ad4e38d1426bc31907981f15
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-04-24 01:07:02 +0530

    Added solution - LeetHub

---
+++ Partition a Linked List around a given value - GFG/partition-a-linked-list-around-a-given-value.py
@@ -0,0 +1,84 @@
+#User function Template for python3
+
+"""
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+
+"""
+class Solution:
+    def partition(self, head, x):
+        temp = head
+        arr = []
+        while temp:
+            arr.append(temp.data)
+            temp = temp.next
+        f = []
+        b = []
+        c = []
+        for j in arr:
+            if j<x:
+                f.append(j)
+            elif j==x:
+                c.append(j)
+            else:
+                b.append(j)
+        arr = f + c + b
+        temp = head
+        i = 0
+        while temp:
+            temp.data = arr[i]
+            i += 1
+            temp = temp.next
+        return head
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        # self.tail
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            # arr.append(str(temp.data))
+            temp = temp.next
+        print()
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = input()
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+        k = int(input())
+        new_head = LinkedList()
+        ob=Solution()
+        new_head = ob.partition(llist.head, k)
+        llist.head = new_head
+        llist.printList()
+        t -= 1
+
+
+
+
+# } Driver Code Ends
