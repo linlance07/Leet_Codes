7b1507d632a9570f4fc20fce94c35a409a57c449
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-05-11 01:33:32 +0530

    Time: 160 ms (23.08%), Space: 13.8 MB (99.62%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        global ans
+        ans = []
+        def combi(s,path,ind):
+            if s>target:
+                return
+            if s==target:
+                ans.append(path)
+                return
+            for _ in range(ind,len(candidates)):
+                combi(s+candidates[_],path+[candidates[_]],_)
+            
+            
+        combi(0,[],0)  #sum,path,index
+        return ans
