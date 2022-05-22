4b722703e8cfd7a3420a9993963ed7cb52abf6de
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-05-23 00:02:26 +0530

    Time: 966 ms (12.64%), Space: 52 MB (5.07%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,33 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        node = [True for _ in range(n)]
+        G = defaultdict(list)
+        for j in range(len(connections)):
+            a,b = connections[j]
+            G[a].append(b)
+            G[b].append(a)
+        global avail
+        free = set()
+        avail = 0
+        def dfs(q):
+            global avail
+            node[q] = False
+            for _ in G[q]:
+                if node[_]:
+                    free.add((min(q,_),max(q,_)))
+                    dfs(_)  
+                else:
+                    if (min(q,_),max(q,_)) not in free:
+                        avail += 1
+        count = 0
+        for x in range(n):
+            if node[x]:
+                count += 1
+                dfs(x)
+        separate = count - 1
+        print(avail,separate)
+        avail //= 2
+        if avail>= separate:
+            return separate
+        return -1
+        
