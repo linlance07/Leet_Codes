d8511037dc6e705299dd83fccc2b972304990b07
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-07-18 23:00:15 +0530

    Time: 268 ms (43.08%), Space: 15.1 MB (58.78%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,15 +1,18 @@
 class Solution:
-    def jump(self, nums: List[int]) -> int:
-        if len(nums)<=1:
-            return 0
+    def jump(self, B: List[int]) -> int:
         l = 0
-        r = nums[0]
+        r = B[0]
         ans = 1
-        while r<len(nums)-1:
+        A = len(B)
+        if A<=1:
+            return 0
+        while r<A-1:
             maxi = 0
             for j in range(l,r+1):
-                maxi = max(maxi,j + nums[j])
+                maxi = max(maxi,j+B[j])
+            if maxi==0 and r!=A-1:
+                return -1
+            ans += 1
             l = r
             r = maxi
-            ans += 1
         return ans
