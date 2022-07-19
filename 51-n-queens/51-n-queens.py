99f0826d57835f8c1f7aab2c48ac7c9a9959c4d4
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-07-19 12:57:22 +0530

    Time: 82 ms (66.13%), Space: 14.5 MB (12.81%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        if n==1:
+            return [['Q']]
+        if n<=3:
+            return []
+        cols = set()
+        posd = set()
+        negd = set()
+        ans = []
+        def dfs(r,board):
+            nonlocal ans
+            if r==n:
+                B = ["".join(row) for row in board]
+                ans.append(B)
+                return
+            for i in range(n):
+                if i not in cols and r+i not in posd and r-i not in negd:
+                    board[r][i] = 'Q'
+                    cols.add(i)
+                    posd.add(r+i)
+                    negd.add(r-i)
+                    dfs(r+1,board)
+                    board[r][i] = '.'
+                    cols.remove(i)
+                    posd.remove(r+i)
+                    negd.remove(r-i)   
+        board = [['.' for __ in range(n)] for _ in range(n)]
+        dfs(0,board)
+        return ans
+                
+            
+            
