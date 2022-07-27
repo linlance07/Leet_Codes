b5dc036969ab0b2257eedd57d88131378cd32bcf
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-07-27 11:07:23 +0530

    Time: 927 ms (95.58%), Space: 32.9 MB (76.66%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
@@ -2,7 +2,9 @@ class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
         ans = 0
         mask = 0
-        for i in range(31,-1,-1):
+        maxi = max(nums)
+        n = len(bin(maxi)[2:])
+        for i in range(n,-1,-1):
             mask |= (1<<i)
             pref = {mask & num for num in nums}
             print(mask,bin(mask)[2:])
