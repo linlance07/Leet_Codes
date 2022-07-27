793d329cc66c8be15b80a05fbca75d39a73b04cb
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-07-27 11:10:49 +0530

    Time: 1399 ms (80.53%), Space: 33.2 MB (76.52%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
@@ -4,14 +4,15 @@ class Solution:
         mask = 0
         maxi = max(nums)
         n = len(bin(maxi)[2:])
-        for i in range(n,-1,-1):
+        for i in range(n-1,-1,-1):
             mask |= (1<<i)
             pref = {mask & num for num in nums}
             print(mask,bin(mask)[2:])
             print(pref)
-            print(ans)
+            
             t = ans | (1<<i)
             for p in pref:
                 if p^t in pref:
                     ans = t
+            print(ans,t)
         return ans
