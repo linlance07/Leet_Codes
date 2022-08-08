3cf36f13f21583e8b7d73e264bce9b0bf4124c0a
Colin Rolance D <79473439+linlance07@users.noreply.github.com>
2022-08-09 01:01:55 +0530

    Time: 508 ms (17.25%), Space: 15 MB (6.27%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,3 +1,12 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        return list(set((permutations(nums))))
+        ans = []
+        vis = set()
+        def perm(arr,path):
+            if len(arr)==0 and tuple(path) not in vis:
+                ans.append(path)
+                vis.add(tuple(path))
+            for i in range(len(arr)):
+                perm(arr[:i]+arr[i+1:],path+[arr[i]])    
+        perm(nums,[])
+        return ans
